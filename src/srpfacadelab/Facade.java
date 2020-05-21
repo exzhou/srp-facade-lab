b619bdd50a6985c04fa75521357c5335846f5c5e
exzhou <ericz1617@gmail.com>
2020-05-21 15:59:56 -0700

    Final Update

--- src/srpfacadelab/Facade.java
+++ src/srpfacadelab/Facade.java
@@ -6,27 +6,27 @@ public class Facade {
     public RpgPlayer player;
     
     //Facade for the other class functions
-    public void useItem(Item item) {
-        return player.Items.useItem(item);
+    public void useItem(Item item, RpgPlayer player) {
+        return Items.useItem(item);
     }
 
-    public boolean pickUpItem(Item item) {
-        return player.Items.pickUpItem(item, player);
+    public boolean pickUpItem(Item item, RpgPlayer player) {
+        return Items.pickUpItem(item, player);
     }
 
-    public void calculateStats() {
-        return player.Inventory.calculateStats(player);
+    public void calculateStats(RpgPlayer player) {
+        return Inventory.calculateStats(player);
     }
 
-    public boolean checkIfItemExistsInInventory(Item item) {
-        return player.Inventory.checkIfItemExistsInInventory(player, item);
+    public boolean checkIfItemExistsInInventory(Item item, RpgPlayer player) {
+        return Inventory.checkIfItemExistsInInventory(player, item);
     }
 
-    public int calculateInventoryWeight() {
-        return player.Inventory.calculateInventoryWeight(player);
+    public int calculateInventoryWeight(RpgPlayer player) {
+        return Inventory.calculateInventoryWeight(player);
     }
 
-    public void takeDamage(int damage) {
-        player.Damage.takeDamage(damage, player);
+    public void takeDamage(int damage, RpgPlayer player) {
+        Damage.takeDamage(damage, player);
     }
 }
--- src/srpfacadelab/RpgPlayer.java
+++ src/srpfacadelab/RpgPlayer.java
@@ -70,32 +70,32 @@ public class RpgPlayer {
 }
 
 
-public class Facade {
+// public class Facade {
 
-    public RpgPlayer player;
+//     public RpgPlayer player;
     
-    //Facade for the other class functions
-    public void useItem(Item item) {
-        return player.Items.useItem(item);
-    }
+//     //Facade for the other class functions
+//     public void useItem(Item item) {
+//         return player.Items.useItem(item);
+//     }
 
-    public boolean pickUpItem(Item item) {
-        return player.Items.pickUpItem(item, player);
-    }
+//     public boolean pickUpItem(Item item) {
+//         return player.Items.pickUpItem(item, player);
+//     }
 
-    public void calculateStats() {
-        return player.Inventory.calculateStats(player);
-    }
+//     public void calculateStats() {
+//         return player.Inventory.calculateStats(player);
+//     }
 
-    public boolean checkIfItemExistsInInventory(Item item) {
-        return player.Inventory.checkIfItemExistsInInventory(player, item);
-    }
+//     public boolean checkIfItemExistsInInventory(Item item) {
+//         return player.Inventory.checkIfItemExistsInInventory(player, item);
+//     }
 
-    public int calculateInventoryWeight() {
-        return player.Inventory.calculateInventoryWeight(player);
-    }
+//     public int calculateInventoryWeight() {
+//         return player.Inventory.calculateInventoryWeight(player);
+//     }
 
-    public void takeDamage(int damage) {
-        player.Damage.takeDamage(damage, player);
-    }
-}
+//     public void takeDamage(int damage) {
+//         player.Damage.takeDamage(damage, player);
+//     }
+// }
